7d142d0631e85275f933d63133bffc1801c37db7
HerVal20 <herval20@htl-shkoder.com>
2026-03-04 12:40:29 +0100

    sec commi

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,18 +11,23 @@ import background from '$lib/assets/phone.jpg'
 
 
 
+<svelte:head>
 
+<title>Hera Electronics - Tech starts here</title>
+<meta name="description" content="Best quality phones, new and used. We sell online only"><meta> 
+</svelte:head>
 
 
 
 <header class="h-[500px] bg-center bg-cover flex items-center justify-center" style="background-image: url({background})">
 
-    <h1 class="text-5xl">Electronic Devices</h1>
+    <h1 class="text-5xl text-blue-600">Electronic Devices</h1>
 
 </header>
 
 
 <main>
+
 
 
 </main>
